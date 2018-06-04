d606f10faaa65c92c9e049a01618624c25de948c
danedelions <danielaechavez@gmail.com>
2018-06-04 10:38:02 +0800

    some stuff

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    // public function getLoginView()
+    // {
+
+    // 	return view('cleanlogin');
+    // }
+
+    // public function doLogin(Request $request){
+
+    // 	//dd($request->toArray());
+    // 	if(Auth::attempt(['firstname' => $request['firstname'], 'password' => $request['password']])){
+
+    // 		return redirect('welcome');
+
+    // 	} else {
+
+    // 		return redirect('/')->with('errorMsg', 'User is not registered');
+    // 	}
+    // }
 }
